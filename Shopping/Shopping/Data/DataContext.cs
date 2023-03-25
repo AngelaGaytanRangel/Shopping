4e2ea0f02c325d43c56f053dbac408db9810c3c8
Angela Gaytan <agaytan@myrobal.com>
2023-03-24 21:26:30 -0600

    Countries Created

--- Shopping/Shopping/Data/DataContext.cs
+++ Shopping/Shopping/Data/DataContext.cs
@@ -1,8 +1,21 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Shooping.Data.Entities;
 
 namespace Shopping.Data
 {
     public class DataContext :DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
     }
 }

c9a0547d442b328918143cde4e507e2c9efd0e3d
Angela Gaytan <agaytan@myrobal.com>
2023-03-27 10:08:26 -0600

    Index User Pending

--- Shopping/Shopping/Data/Entities/User.cs
+++ Shopping/Shopping/Data/Entities/User.cs
@@ -37,7 +37,7 @@ namespace Shopping.Data.Entities
         [Display(Name = "Foto")]
         public string ImageFullPath => ImageId == Guid.Empty
             ? $"https://localhost:7057/images/noimage.png"
-            : $"https://shoppingzulu.blob.core.windows.net/users/{ImageId}";
+            : $"https://shoppinggaytan.blob.core.windows.net/users/{ImageId}";
 
         [Display(Name = "Tipo de usuario")]
         public UserType UserType { get; set; }

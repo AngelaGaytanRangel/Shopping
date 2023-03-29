b8bcefdb6943c40965ba50a586931e4deee9ad5f
Angela Gaytan <agaytan@myrobal.com>
2023-03-29 13:21:36 -0600

    Add detail in home.

--- Shopping/Shopping/Controllers/HomeController.cs
+++ Shopping/Shopping/Controllers/HomeController.cs
@@ -4,8 +4,11 @@ using Shopping.Data;
 using Shopping.Data.Entities;
 using Shopping.Helpers;
 using Shopping.Models;
+using Shopping.Data.Entities;
+using Shopping.Data;
+using Shopping.Helpers;
+using Shopping.Models;
 using System.Diagnostics;
-
 namespace Shopping.Controllers
 {
     public class HomeController : Controller
@@ -13,14 +16,12 @@ namespace Shopping.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly DataContext _context;
         private readonly IUserHelper _userHelper;
-
         public HomeController(ILogger<HomeController> logger, DataContext context, IUserHelper userHelper)
         {
             _logger = logger;
             _context = context;
             _userHelper = userHelper;
         }
-
         public async Task<IActionResult> Index()
         {
             List<Product> products = await _context.Products
@@ -28,9 +29,7 @@ namespace Shopping.Controllers
                 .Include(p => p.ProductCategories)
                 .OrderBy(p => p.Description)
                 .ToListAsync();
-
             HomeViewModel model = new() { Products = products };
-
             User user = await _userHelper.GetUserAsync(User.Identity.Name);
             if (user != null)
             {
@@ -38,23 +37,103 @@ namespace Shopping.Controllers
                     .Where(ts => ts.User.Id == user.Id)
                     .SumAsync(ts => ts.Quantity);
             }
-
             return View(model);
         }
-
         public async Task<IActionResult> Add(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            Product product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            User user = await _userHelper.GetUserAsync(User.Identity.Name);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            TemporalSale temporalSale = new()
+            {
+                Product = product,
+                Quantity = 1,
+                User = user
+            };
+            _context.TemporalSales.Add(temporalSale);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [Route("error/404")]
+        public IActionResult Error404()
+        {
+            return View();
+        }
 
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Product product = await _context.Products
+                .Include(p => p.ProductImages)
+                .Include(p => p.ProductCategories)
+                .ThenInclude(pc => pc.Category)
+                .FirstOrDefaultAsync(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            string categories = string.Empty;
+            foreach (ProductCategory category in product.ProductCategories)
+            {
+                //categories += $"{category.Category.Name}, ";
+                categories = "Vacio";
+            }
+            categories = categories.Substring(0, categories.Length - 2);
+
+            AddProductToCartViewModel model = new()
+            {
+                Categories = categories,
+                Description = product.Description,
+                Id = product.Id,
+                Name = product.Name,
+                Price = product.Price,
+                ProductImages = product.ProductImages,
+                Quantity = 1,
+                Stock = product.Stock,
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Details(AddProductToCartViewModel model)
+        {
             if (!User.Identity.IsAuthenticated)
             {
                 return RedirectToAction("Login", "Account");
             }
 
-            Product product = await _context.Products.FindAsync(id);
+            Product product = await _context.Products.FindAsync(model.Id);
             if (product == null)
             {
                 return NotFound();
@@ -69,30 +148,14 @@ namespace Shopping.Controllers
             TemporalSale temporalSale = new()
             {
                 Product = product,
-                Quantity = 1,
+                Quantity = model.Quantity,
+                Remarks = model.Remarks,
                 User = user
             };
 
             _context.TemporalSales.Add(temporalSale);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-
-        }
-        [Route("error/404")]
-        public IActionResult Error404()
-        {
-            return View();
         }
     }
 }

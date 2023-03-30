e9c12d0771bb755c57f135846342f79d861f05af
Angela Gaytan <agaytan@myrobal.com>
2023-03-30 09:07:56 -0600

    My Orders.

--- Shopping/Shopping/Controllers/OrdersController.cs
+++ Shopping/Shopping/Controllers/OrdersController.cs
@@ -10,7 +10,7 @@ using Shopping.Helpers;
 
 namespace Shopping.Controllers
 {
-    [Authorize(Roles = "Admin")]
+    
     public class OrdersController : Controller
     {
         private readonly DataContext _context;
@@ -23,6 +23,7 @@ namespace Shopping.Controllers
             _flashMessage = flashMessage;
             _ordersHelper = ordersHelper;
         }
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Index()
         {
             return View(await _context.Sales
@@ -31,6 +32,7 @@ namespace Shopping.Controllers
                 .ThenInclude(sd => sd.Product)
                 .ToListAsync());
         }
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -51,6 +53,7 @@ namespace Shopping.Controllers
 
             return View(sale);
         }
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Dispatch(int? id)
         {
             if (id == null)
@@ -78,7 +81,7 @@ namespace Shopping.Controllers
 
             return RedirectToAction(nameof(Details), new { Id = sale.Id });
         }
-
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Send(int? id)
         {
             if (id == null)
@@ -106,6 +109,7 @@ namespace Shopping.Controllers
 
             return RedirectToAction(nameof(Details), new { Id = sale.Id });
         }
+        [Authorize(Roles = "Admin")]
 
         public async Task<IActionResult> Confirm(int? id)
         {
@@ -134,7 +138,7 @@ namespace Shopping.Controllers
 
             return RedirectToAction(nameof(Details), new { Id = sale.Id });
         }
-
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Cancel(int? id)
         {
             if (id == null)
@@ -159,6 +163,38 @@ namespace Shopping.Controllers
             }
 
             return RedirectToAction(nameof(Details), new { Id = sale.Id });
+        }
+        [Authorize(Roles = "User")]
+        public async Task<IActionResult> MyOrders()
+        {
+            return View(await _context.Sales
+                .Include(s => s.User)
+                .Include(s => s.SaleDetails)
+                .ThenInclude(sd => sd.Product)
+                .Where(s => s.User.UserName == User.Identity.Name)
+                .ToListAsync());
+        }
+
+        [Authorize(Roles = "User")]
+        public async Task<IActionResult> MyDetails(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Sale sale = await _context.Sales
+                .Include(s => s.User)
+                .Include(s => s.SaleDetails)
+                .ThenInclude(sd => sd.Product)
+                .ThenInclude(p => p.ProductImages)
+                .FirstOrDefaultAsync(s => s.Id == id);
+            if (sale == null)
+            {
+                return NotFound();
+            }
+
+            return View(sale);
         }
 
     }
